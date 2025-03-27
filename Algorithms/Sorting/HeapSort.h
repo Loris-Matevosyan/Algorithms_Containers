0e6717560b872e839315420abed0b6e2845624ae
LorMat <dr_matevosyan@yahoo.com>
2025-03-27 14:28:34 +0400

    minor changes

--- Algorithms/Sorting/HeapSort.h
+++ Algorithms/Sorting/HeapSort.h
@@ -66,7 +66,7 @@ public:
             heapify(array, size, i);
         }
 
-        for( int i = size - 1; i >= 0; --i ) 
+        for( int i = size - 1; i > 0; --i ) 
         {
             swap(array[0], array[i]);
 
